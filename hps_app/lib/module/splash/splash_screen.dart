1742871d5fe6271dafd38e001cc76d423ef1daf2
thanhvinh <vinhdev.pixel@gmail.com>
2025-04-02 10:51:49 +0700

    feat(register): create register screen

--- hps_app/lib/module/splash/splash_screen.dart
+++ hps_app/lib/module/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:hps_app/module/login/login_screen.dart';
+import 'package:hps_app/module/register/register.dart';
 import 'package:hps_app/shared/constants/colors.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -83,7 +84,7 @@ class _SplashScreenState extends State<SplashScreen> {
                           Navigator.pushReplacement(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => LoginScreen(),
+                              builder: (context) => RegisterScreen(),
                             ),
                           );
                         },
