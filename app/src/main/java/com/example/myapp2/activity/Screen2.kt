90d83cb60ea7354ad5a55720786ee83c9a16ae5f
haint <ggrreenn003@gmail.com>
2025-06-18 17:10:56 +0700

    feat(android-components): add demos for activity, service, broadcast receiver, content provider, and intent

---
+++ app/src/main/java/com/example/myapp2/activity/Screen2.kt
@@ -0,0 +1,23 @@
+package com.example.myapp2.activity
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapp2.R
+
+class Screen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_screen2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        Log.d("Activity2", "Screen 2 called ${this.javaClass.simpleName}")
+    }
+}
